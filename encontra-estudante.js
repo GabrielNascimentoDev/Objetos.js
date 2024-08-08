c230c2734c0f7aef7276db81ed1ef7830173366e
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-08-08 10:55:17 -0300

    arrumando encontra-estudantes.js

--- encontra-estudante.js
+++ encontra-estudante.js
@@ -4,15 +4,14 @@ function buscaInformacao(lista, chave, valor) {
     return lista.find((estudante) => estudante[chave].includes(valor));
 }
 
-const estudanteEncontrado = buscaInformacao(estudantes, "nome", "Oralle")
+const estudanteEncontrado = buscaInformacao(estudantes, "nome", "Oralle");
 
 console.log(estudanteEncontrado);
 
-const emailEstudante = buscaInformacao(estudantes, "email", "olippini0@deviantart.com")
+const emailEstudante = buscaInformacao(estudantes, "email", "olippini0@deviantart.com");
 
 console.log(emailEstudante);
 
-const telefoneEstudante = buscaInformacao(estudantes, "telefone", "7919437785")
+const telefoneEstudante = buscaInformacao(estudantes, "telefone", "7919437785");
 
 console.log(telefoneEstudante);
-
