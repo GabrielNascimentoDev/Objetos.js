4ea12ab24a2aa6cdb591ca12be0c42f37f246549
GabrielNascimentoDev <biel.henrique.s.n@gmail.com>
2024-08-09 11:32:55 -0300

    arrumando converte_json.js

--- converte-json.js
+++ converte-json.js
@@ -1,9 +1,7 @@
 const estudante = require("./estudante.json");
 
-//CONVERTENDO EM STRING
 const stringEstudante = JSON.stringify(estudante);
 
-//CONVERTENDO EM OBJETO NOVAMENTE
 const objEstudante = JSON.parse(stringEstudante)
 
 console.log(objEstudante);
